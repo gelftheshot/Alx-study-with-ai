24ac05e1d5076d262fc7d8b7dcaa20a514b7daa1
lihon <gelf.learing@gmail.com>
2024-08-17 22:16:42 +0300

    adding the fav icon to my website

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,8 @@ import theme from '../styles/theme';
 import Header from '../components/Header';
 import UserInitializer from '../components/UserInitializer';
 import Footer from '../components/Footer';
-
 const inter = Inter({ subsets: ["latin"] });
-
+import Head from "next/head";
 export const metadata = {
   title: "FlashCard Pro - Create Flashcards Easily",
   description: "Turn your text into flashcards with our AI-powered tool",
@@ -19,6 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <title>flashcard</title>
+          <link rel="icon" href="./favicon.ico"/>
+        </Head>
         <body className={inter.className}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
