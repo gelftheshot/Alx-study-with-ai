d1c0f834b680d5fd061e2a7953361bd27654e186
lihon <gelf.learing@gmail.com>
2024-08-17 23:21:44 +0300

    protecting my routes

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-import { createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(['/create(.*)', '/my-flashcards(.*)'])
-export default clerkMiddleware()
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!.*\\..*|_next).*)',
+    '/',
     '/(api|trpc)(.*)',
   ],
 }
